8856e2f9226e0d4875652c81a83d502fdef2bf81
live-in-nakameguro <sorakitahara2020@gmail.com>
2023-02-14 03:17:43 +0900

    ToDo component base

--- apps/todolist/todo-react-app/ClientApp/src/components/Todo.js
+++ apps/todolist/todo-react-app/ClientApp/src/components/Todo.js
@@ -1,22 +1,75 @@
-// Todoコンポーネント
-export const Todo = () => {
-    return (
-      <div>
-        <h1>Todoリスト</h1>
-        <input type="text" />
-        <button>追加</button>
-        <ul>
-            <li>
-              <input type="checkbox" />
-              <span>プログラミング</span>
-              <button>削除</button>
-            </li>
-            <li>
-              <input type="checkbox" />
-              <span>ランニング</span>
-              <button>削除</button>
-            </li>
-        </ul>
-      </div>
-    );
-  };
+import React from 'react';
+
+const initToDoList = [
+    {
+      id: 1,
+      name: "教える",
+      isComplete: false,
+    },
+    {
+      id: 2,
+      name: "ベースを作る",
+      isComplete: true,
+    },
+];
+  
+export class Todo extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            toDoList: initToDoList
+        };
+    }
+
+    changeToDoStatus = id => {
+        let changeToDoList = [];
+        this.state.toDoList.map((todo) => {
+            if(todo.id == id){
+                todo.isComplete = !todo.isComplete;
+            }
+            changeToDoList.push(todo);
+        })
+        this.setState({toDoList: changeToDoList});
+    }
+
+    showToDoList = () => {
+        return (
+            this.state.toDoList.map((todo) => (
+                <li key={todo.id}>
+                    <input 
+                        type="checkbox"
+                        onClick={() => this.changeToDoStatus(todo.id)}
+                        checked={todo.isComplete}
+                    />
+                    {todo.isComplete ? (
+                        <span style={{ textDecorationLine: "line-through" }}>
+                            {todo.name}
+                        </span>
+                        ) : (
+                        <span>
+                            {todo.name}
+                        </span>
+                    )}
+                    <button>
+                        delete
+                    </button>
+                </li>
+              ))
+        );
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Todo List</h1>
+                <div>
+                    <input type="text" />
+                    <button>
+                        Add
+                    </button>
+                </div>
+                {this.showToDoList()}
+            </div>
+        );
+    }
+}
